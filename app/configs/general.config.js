c184b71dacc1e4bc64b5d83820f51c3284d611a3
BigLcorp <127101806+BigLcorp@users.noreply.github.com>
2023-08-17 10:23:28 +0700

    new 2

--- app/configs/general.config.js
+++ app/configs/general.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    DBConnectors: {
-        host: process.env.DB_HOST || "db4free.net",
-        port: process.env.DB_PORT || 3306,
-        username: process.env.DB_USER || "audiobook",
-        password: process.env.DB_PASSWORD || "thanhbinh88",
-        database: process.env.DB_NAME || "audiobook",
-        dialect: process.env.DB_DIALECT || "mysql",
-    },
-    jwtAuthKey: 'abcxyz',
-    tokenLoginExpiredDays:'25 days'
+  DBConnectors: {
+    host: process.env.DB_HOST || "db4free.net",
+    port: process.env.DB_PORT || 3306,
+    username: process.env.DB_USER || "someguy46",
+    password: process.env.DB_PASSWORD || "ny9tgxj1xd55ywcejmg5",
+    database: process.env.DB_NAME || "api_test",
+    dialect: process.env.DB_DIALECT || "mysql",
+  },
+  jwtAuthKey: "abcxyz",
+  tokenLoginExpiredDays: "25 days",
 };
